c303a7aded95f32121bf4324a985d03d094181c0
Guy Golan <ggolan@technion.ac.il>
2023-07-05 09:19:34 +0300

    First commit

---
+++ springboot-postgresql-hibernet-crud-example/src/main/java/netjavaguides/springboot/SpringbootPostgresqlHibernetCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package netjavaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresqlHibernetCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresqlHibernetCrudExampleApplication.class, args);
+	}
+
+}
---
+++ springboot-postgresql-hibernet-crud-example/src/test/java/netjavaguides/springboot/SpringbootPostgresqlHibernetCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package netjavaguides.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresqlHibernetCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
